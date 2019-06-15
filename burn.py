5321644cc40cb962c9fe34a1b5bba2a96e7d5f93
fabiananicchio <51458690+fabiananicchio@users.noreply.github.com>
2019-06-15 10:26:27 -0300

    Update burn.py

--- burn.py
+++ burn.py
@@ -7,49 +7,45 @@ data = plyfile.PlyData.read('germany.ply')['vertex']
 xyz = np.c_[data['x'], data['y'], data['z']]          # Eixos
 rgb = np.c_[data['red'], data['green'], data['blue']] # RGB
 n = np.c_[data['nx'], data['ny'], data['nz']]         # Normais
-
+params = [4, 7, 4, 9, 6, 12, 18, 15, 3, 10, 11, 12, 13, 14, 3, 4, 5, 15, 14, 13]
 
 def printmodel():
-	v = pptk.viewer(xyz)
+	
 	v.set(point_size=0.005)
 	v.attributes(rgb / 255., 0.5 * (1 + n))
 	return
 
-# extractedData = rgb[:,[2]]  # gets x axis
-
-# matrix = 1*[1*[256]]
-# print(matrix)
-# rgb[:,[2]][0] = matrix
-# print(rgb[:,[2]])
+v = pptk.viewer(xyz)
 
 P = np.array([[1, 2, 3],
 		   [4, 5, 6],
 		   [7, 8, 9],
 		   [10, 11, 12]])
 
-y=1
-for y in range(10):
+#y=1
+for y in range(20):
 	if y ==0:
 		y=1
 	for x in range(len(rgb)): 
 		if x == 0:
 			x=1
-		w = y * 200000
+		w = params[y] * 100000
 # primeira camada - vermelho
 		if (x < w):
 			rgb[x,0] = 255
-			rgb[x,1] = 0
-			rgb[x,2] = 0
+			#rgb[x,1] = 0
+			#rgb[x,2] = 0
 # segunda camada - laranja
-		if (x < (w-200000)):
+		if (x < (w-100000)):
 			rgb[x,0] = 250
 			rgb[x,1] = 170
-			rgb[x,2] = 0
+			#rgb[x,2] = 0
 # terceira camada - preto
-		if (x < (w-400000)):
+		if (x < (w-200000)):
 			rgb[x,0] = 0
 			rgb[x,1] = 0
-			rgb[x,2] = 0
+			rgb[x,2] = 0			
+		x+=1
 	printmodel()	
-	print("fase "+ str(y))
+	print("fase "+ str(y) + " - " + str(params[y]))
 
